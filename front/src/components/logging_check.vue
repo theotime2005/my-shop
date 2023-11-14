5b69a9a7011d6cf3316cb13cadcffc96c6dcb29e
theotime2005 <theotime.berthod@epitech.digital>
2023-11-14 22:19:23 +0100

    Mise en place de la page, il faut maintenant faire les conditions

---
+++ front/src/components/logging_check.vue
@@ -0,0 +1,41 @@
+<script>
+let token;
+function test_logging(data, token) {
+  return new Promise(resolve, reject) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', // Vous pouvez ajuster le type de contenu en fonction de vos besoins
+      },
+      body: JSON.stringify(donnees)
+    }
+    fetch(url, options)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`La requête a échoué avec le statut ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          token = data;
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+  });
+}
+  }
+}
+export default {
+  name: "logging_check",
+}
+</script>
+
+<template>
+  <p>Loading...</p>
+</template>
+
+<style scoped>
+
+</style>
