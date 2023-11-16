84d838c129bd75b7e49dcae1d0f2fba6b4ab2f90
theotime2005 <theotime.berthod@epitech.digital>
2023-11-16 16:45:18 +0100

    Mise en place du bouton burger utilisable de maniere accessible, il faudra refaire le style

---
+++ front/src/components/FlashMessage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="live-region" v-show="isVisible">{{ message }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: '',
+      isVisible: false,
+    };
+  },
+  methods: {
+    displayFlash(message, reload) {
+      this.message = message;
+      this.isVisible = true;
+
+      setTimeout(() => {
+        this.message = '';
+        this.isVisible = false;
+      }, 3000);
+
+      if (reload) {
+        this.$router.push(reload);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+#live-region {
+  display: none;
+  /* Ajoutez les styles nécessaires pour le message flash */
+}
+</style>
